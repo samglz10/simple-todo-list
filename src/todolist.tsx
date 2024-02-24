808c66a0c5a84ac72c9535253b323ba07b89d888
Samuel Gonzalez <samglz10@gmail.com>
2024-02-23 20:14:49 -0500

    will not delete tasks

--- src/todolist.tsx
+++ src/todolist.tsx
@@ -1,41 +1,56 @@
-import React from "react"
-import { useState } from "react";
+import React from 'react'
+import { useState, useEffect } from "react";
 
 const TodoList = ()=>{
-    const [todos, setTodos] = useState("");
+    const [todos, setTodos] = useState('');
     const [todoArr, setTodoArr]= useState([]);
-    const change =(e)=>{
+
+    const handleChange =(e)=>{
         const newValue = e.target.value
         setTodos(newValue);
     }
-    const submit = (e)=>{
+
+    const handleSubmit = (e)=>{
         setTodoArr([todos, ...todoArr])
+        localStorage.setItem('todos', JSON.stringify([...todoArr]));
     }
 
-    const deleteTask = (e)=>{
-        
+    const handleDeleteTask = (index)=>{
+        let removeTodo = [...todoArr];
+        removeTodo.slice(index, 0);
+        localStorage.setItem('todos', JSON.parse([...todoArr]))
+        setTodos(removeTodo);
+    
+
     }
+    //watch for localstorage and changes
+    useEffect(()=>{
+        let storedTodo = JSON.parse(localStorage.getItem('todos'));
+        if(storedTodo){
+            setTodoArr(storedTodo);
+        }
+    },[])
+
     return (
         <div>
             <h1>Vite + React Todo List </h1>
             <div className="main-container">
             <div className="input-container">
-                <input type="text" placeholder="add a new todo" value={todos} onChange={change}></input>
-                <button className="primary-btn"type="submit" onClick={submit}> Add task</button>
+                <input type="text" placeholder="add a new todo" value={todos} onChange={handleChange}></input>
+                <button className="primary-btn"type="submit" onClick={handleSubmit}> Add task</button>
             </div>
             <ul>
                 {todoArr.map((todo, index)=>{
                     return (
                         <div className="todo-wrapper">
                             <li key={index}>{todo}</li>
-                            <button type="submit"  className="secondary-btn"> Delete </button>
+                            <button type="submit" onClick={handleDeleteTask} className="secondary-btn"> Delete </button>
                         </div>
                     );
                 })}
             </ul>
             </div>
         </div>
-    
     )
 }
 export default TodoList
