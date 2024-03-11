00bfed0482d5e1582dde7860ea5cf94a9395bb23
Samuel Gonzalez <samglz10@gmail.com>
2024-03-10 22:24:50 -0400

    converted todos from strings to objects

--- src/todolist.tsx
+++ src/todolist.tsx
@@ -2,49 +2,54 @@ import React from 'react'
 import { useState, useEffect } from "react";
 
 const TodoList = ()=>{
-    const [newTodo, setNewTodo] = useState('');
+    const [done, setDone] = useState(false);
+    const [newTodo, setNewTodo] = useState({task:'', done: done });
     const [todos, setTodos]= useState([]);
 
     const handleChange =(e)=>{
         const newValue = e.target.value
-        setNewTodo(newValue);
+        setNewTodo(prevTodo=> ({...prevTodo, task: newValue, done: done}));
     }
-
-    const handleSubmit = (e)=>{
+    const handleComplete = ()=>{
+        setDone(done => !done)
+        console.log(done)
+    }
+    const handleSubmit = ()=>{
         setTodos([newTodo, ...todos])
+        console.log(newTodo)
         localStorage.setItem('todos', JSON.stringify([...todos]));
-        setNewTodo('');
-
+        
     }
 
     const handleDeleteTask = (index)=>{
         let removeTodo = [...todos];
         removeTodo.splice(index, 1);
-        console.log(removeTodo);
         localStorage.setItem('todos', JSON.stringify(removeTodo))
         setTodos(removeTodo);
     }
+
     //watch for localstorage and changes
     useEffect(()=>{
         let storedTodo = JSON.parse(localStorage.getItem('todos'));
         if(storedTodo){
             setTodos(storedTodo);
         }
-    },[])
+    },[done])
 
     return (
         <div>
             <h1>Vite + React Todo List </h1>
             <div className="main-container">
                 <div className="input-container">
-                    <input type="text" placeholder="Add a task" value={newTodo} onChange={handleChange}></input>
+                    {/** */}
+                    <input type="text" placeholder="Add a task" value={newTodo.task} onChange={handleChange}  ></input>
                     <button className="primary-btn"type="submit" onClick={handleSubmit}> Add task</button>
                 </div>
             <ul>
                 {todos.map((todo, index)=>{
                     return (
                         <div className="todo-wrapper" key={index}>
-                            <li >{index}{todo}</li>
+                            <li onClick={handleComplete} className="todos" >{todo.task}</li>
                             <button type="submit" onClick={()=>{handleDeleteTask(index)}} className="secondary-btn"> Delete </button>
                         </div>
                     );
